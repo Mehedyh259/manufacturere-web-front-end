4c3e11b3913c9bfef1b435a9436bbe75644ab9a4
Mehedyh259 <mehedyh259@gmail.com>
2022-05-22 01:38:41 +0600

    navbar added with toggle theme color

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
-      <h2>app running</h2>
-      <button className="btn btn-primary">hello</button>
-    </div>
+    <>
+      <Navbar>
+
+
+      </Navbar>
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,34 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   daisyui: {
-      themes: [
-        {
-          mytheme: {
-          
- "primary": "#570DF8",
-          
- "secondary": "#F000B8",
-          
- "accent": "#37CDBE",
-          
- "neutral": "#3D4451",
-          
- "base-100": "#FFFFFF",
-          
- "info": "#3ABFF8",
-          
- "success": "#36D399",
-          
- "warning": "#FBBD23",
-          
- "error": "#F87272",
-          },
+    themes: [
+      {
+        light: {
+          primary: "#570DF8",
+          secondary: "#F000B8",
+          accent: "#37CDBE",
+          neutral: "#3D4451",
+          "base-100": "#FFFFFF",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
         },
-      ],
-    },
-    plugins: [
-      require('daisyui'),
+      },
+      {
+        dark: {
+          primary: "#D9F99D",
+          secondary: "#FDE68A",
+          accent: "#4B5563",
+          neutral: "#F3F4F6",
+          "base-100": "#1F2937",
+          info: "#98A8DD",
+          success: "#1BBB70",
+          warning: "#F59E0B",
+          error: "#FB7185",
+        },
+      },
     ],
-}
+  },
+  plugins: [require("daisyui")],
+};
