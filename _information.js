312b21c2a1f8852e0b890bcdd3e8f86e0008556d
Mehedyh259 <mehedyh259@gmail.com>
2022-05-26 00:29:46 +0600

    read me file modified

---
+++ _information.js
@@ -0,0 +1,13 @@
+/**
+ * Live Site: https://manufacturer-web-a4c58.web.app/
+ * 
+ * Client Git Repo: https://github.com/programming-hero-web-course1/manufacturer-website-client-side-Mehedyh259
+ * 
+ * Server Git Repo: https://github.com/programming-hero-web-course1/manufacturer-website-server-side-Mehedyh259
+ * 
+ * 
+ * 
+ * Admin Access: name= Admin, email= admin@admin.com, password= 123456
+ * 
+ * 
+ */
